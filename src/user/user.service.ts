6743070db6e7c5d4c07c308dd670f14d8c840e42
KusalChamu <chamuchamp09@gmail.com>
2026-01-07 13:06:28 +0530

    add hashing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/createUser.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "./user.entity";
+import { Repository } from "typeorm"
 
 @Injectable()
 export class UserService {
-    createUser(createUserDto): CreateUserDto {
-        return createUserDto;
+    constructor(@InjectRepository(UserEntity) private readonly userRepository:Repository<UserEntity>){}
+    async createUser(createUserDto:CreateUserDto): Promise<CreateUserDto> {
+        const newUser = new UserEntity()
+        Object.assign(newUser,createUserDto)
+
+        return await this.userRepository.save(newUser)
     }
 }
