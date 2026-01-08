dd23d2acee2574b72f41ff261517ded96c566308
KusalChamu <chamuchamp09@gmail.com>
2026-01-08 16:30:35 +0530

    add validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/createUser.dto";
+import { IUserResponse } from "./types/userResponse.interface";
 
 @Controller('users')
 export class UserController {
     constructor(private readonly userService:UserService) {}
+
+    @UsePipes(new ValidationPipe())
     @Post()
     async createUser(@Body("user") createUserDto:CreateUserDto):Promise<any>{
         return await this.userService.createUser(createUserDto);
+    }
+
+    @Post("login")
+    async loginUser(@Body("user") loginUserDto:any): Promise<IUserResponse>{
+        return {} as IUserResponse;
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,38 +1,63 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/createUser.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserEntity } from "./user.entity";
-import { Repository } from "typeorm"
+import { Repository } from "typeorm";
 import { IUserResponse } from "./types/userResponse.interface";
 import { sign } from "jsonwebtoken";
 
 @Injectable()
 export class UserService {
-    constructor(@InjectRepository(UserEntity) private readonly userRepository:Repository<UserEntity>){}
-    async createUser(createUserDto:CreateUserDto): Promise<IUserResponse> {
-        const newUser = new UserEntity()
-        Object.assign(newUser,createUserDto)
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) {}
+
+    async createUser(createUserDto: CreateUserDto): Promise<IUserResponse> {
+
+        // ✅ CHECK EMAIL
+        const userByEmail = await this.userRepository.findOne({
+            where: { email: createUserDto.email }
+        });
+
+        // ✅ CHECK USERNAME
+        const userByUsername = await this.userRepository.findOne({
+            where: { username: createUserDto.username }
+        });
+
+        // ✅ THROW ERROR
+        if (userByEmail || userByUsername) {
+            throw new HttpException(
+                "Email or username already taken",
+                HttpStatus.UNPROCESSABLE_ENTITY
+            );
+        }
+
+        // ✅ CREATE USER
+        const newUser = new UserEntity();
+        Object.assign(newUser, createUserDto);
 
         const savedUser = await this.userRepository.save(newUser);
-        return this.generateUserResponse(savedUser);
-    }
 
-    generateToken(user:UserEntity):string{
+        return this.generateUserResponse(savedUser);
+    }  
+
+    generateToken(user: UserEntity): string {
         return sign(
             {
-                id:user.id,
-                username:user.username,
-                email:user.email,
+                id: user.id,
+                username: user.username,
+                email: user.email,
             },
             process.env.JWT_SECRET,
-        )
+        );
     }
 
-    generateUserResponse(user:UserEntity):IUserResponse{
-        return{
+    generateUserResponse(user: UserEntity): IUserResponse {
+        return {
             user: {
-                ...user,//spread operator(copies all data in the user to new object)
-                token:this.generateToken(user),
+                ...user,
+                token: this.generateToken(user),
             }
         };
     }
